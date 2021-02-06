e21d04fdae2738593b2fc699fc7ea76600613ced
jon-dominguez94 <jon.dominguez94@yahoo.com>
2021-02-06 09:54:13 -0800

    Finish Item class for todo board v1

---
+++ foundations/todo_board/item.rb
@@ -0,0 +1,34 @@
+class Item
+    attr_accessor :title, :deadline, :description
+
+    def self.valid_date?(date_str)
+        pieces = date_str.split('-')
+        return false if pieces.length != 3 || 
+                        pieces[0].length != 4 ||
+                        pieces[1].length != 2 ||
+                        pieces[2].length != 2 ||
+                        pieces.any? {|piece| !all_nums(piece)} ||
+                        pieces[1..2].any? {|p| p.to_i < 0} ||
+                        pieces[1].to_i > 12 ||
+                        pieces[2].to_i > 31
+        true
+    end
+
+    def initialize(title, deadline, description)
+        @title = title
+        @deadline = deadline
+        @description = description
+        raise "Invalid date!" if !self.class.valid_date?(@deadline)
+    end
+
+    def deadline=(date)
+        raise "Invalid date!" if !self.class.valid_date?(date)
+        @deadline = date
+    end
+
+end
+
+def all_nums(str)
+    numbers = ('0'..'9').to_a
+    str.split("").all? {|ch| numbers.include?(ch)}
+end
