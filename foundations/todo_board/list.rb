742eb4fa5dfbb98663d481e0696161bce3b586f8
jon-dominguez94 <jon.dominguez94@yahoo.com>
2021-02-07 23:35:15 -0800

    Finish V1 of todo board

--- foundations/todo_board/list.rb
+++ foundations/todo_board/list.rb
@@ -1,1 +1,94 @@
-list.rb
+require_relative './item.rb'
+
+class List
+    attr_accessor :label
+
+    def initialize(label)
+        @label = label
+        @items = []
+    end
+
+    def add_item(title, deadline, description="")
+        begin
+            item = Item.new(title, deadline, description)
+            @items << item
+        rescue
+            return false
+        end
+        true
+    end
+
+    def size
+        @items.length
+    end
+
+    def valid_index?(idx)
+        idx.between?(0, self.size - 1)
+    end
+
+    def swap(idx1, idx2)
+        return false if [idx1, idx2].any? {|idx| !self.valid_index?(idx)}
+        @items[idx1], @items[idx2] = @items[idx2], @items[idx1]
+        true
+    end
+
+    def [](index)
+        self.valid_index?(index) ? @items[index] : nil
+    end
+
+    def priority
+        self[0]
+    end
+
+    def print
+        puts '---------------------------------------'
+        puts '               ' + @label.upcase + '               '
+        puts '---------------------------------------'
+        puts 'Index | Item              | Deadline   '
+        puts '---------------------------------------'
+        @items.each_with_index do |item, i|
+            count = 18 - item.title.length
+            puts i.to_s + '     | ' + item.title + (" " * count) + "| " + item.deadline
+            # puts output
+        end
+        puts '---------------------------------------'
+    end
+    
+    def print_full_item(index)
+        return if !self.valid_index?(index)
+        puts '---------------------------------------'
+        item = @items[index]
+        count = 29 - item.title.length
+        puts item.title + (" " * count) + item.deadline
+        puts item.description
+        puts '---------------------------------------'
+    end
+
+    def print_priority
+        self.print_full_item(0)
+    end
+
+    def up(index, amount=1)
+        return false if !self.valid_index?(index)
+        while index > 0 && amount > 0
+            @items[index], @items[index - 1] = @items[index-1], @items[index]
+            index -= 1
+            amount -= 1
+        end
+        true 
+    end
+
+    def down(index, amount=1)
+        return false if !self.valid_index?(index)
+        while index < self.size - 1 && amount > 0
+            @items[index], @items[index + 1] = @items[index + 1], @items[index]
+            index += 1
+            amount -= 1
+        end
+        true 
+    end
+
+    def sort_by_date!
+        @items.sort_by!{|item| item.deadline}
+    end
+end
--- foundations/todo_board/todo_board.rb
+++ foundations/todo_board/todo_board.rb
@@ -1,1 +1,43 @@
-todo_board.rb
+require_relative 'list.rb'
+
+class TodoBoard
+    def initialize(label)
+        @list = List.new(label)
+    end
+
+    def get_command
+        puts "Enter a command: "
+        inp, *args = gets.chomp.split
+        case inp
+        when "mktodo"
+            @list.add_item(*args)
+        when "up"
+            args.map!(&:to_i)
+            @list.up(*args)
+        when "down"
+            args.map!(&:to_i)
+            @list.down(*args)
+        when "swap"
+            args.map!(&:to_i)
+            @list.swap(*args)
+        when "sort"
+            @list.sort_by_date!
+        when "priority"
+            @list.print_priority
+        when "print"
+            args[0] ? @list.print_full_item(args[0].to_i) : @list.print
+        when "quit"
+            return false
+        else
+            puts "Invalid command"
+        end
+        true
+    end
+
+    def run
+        running = true
+        while running
+            running = self.get_command
+        end
+    end
+end
