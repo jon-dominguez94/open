0bbb7e393d66709d4ab5044eacb3649e238b1605
jon-dominguez94 <jon.dominguez94@yahoo.com>
2021-02-20 17:06:30 -0800

    Finish full game implementation of ghost

--- ruby/ghost/game.rb
+++ ruby/ghost/game.rb
@@ -3,7 +3,7 @@ require_relative './player.rb'
 
 class Game
 
-    attr_accessor :dictionary, :current_player, :previous_player, :fragment
+    attr_reader :fragment
 
     def initialize(player1, player2)
         # @players = players
@@ -13,6 +13,7 @@ class Game
         @dictionary = Set.new
         @current_player = @player1
         @previous_player = @player2
+        @losses = Hash.new(0)
     end
 
     def populate_dictionary(filename)
@@ -38,14 +39,49 @@ class Game
         @dictionary.include?(@fragment) || !self.fragment_is_prefix?
     end
 
+    def game_over? 
+        @losses.values.any? {|total| total == 5}
+    end
+
+    def translate_losses(count)
+        "GHOST"[0...count] + ("_" * (5 - count))
+    end
+    
+    def print_scores
+        puts @player1.name + ": " + self.translate_losses(@losses[@player1])
+        puts @player2.name + ": " + self.translate_losses(@losses[@player2])
+    end
+    
+    def print_scoreboard
+        puts "-----------------"
+        puts "SCOREDBOARD"
+        puts "-----------------"
+        self.print_scores
+    end
+    
     def play_round
+        puts "-----------------"
+        puts "Fragment: " + @fragment
         puts @current_player.name + "'s turn!"
         @fragment += @current_player.get_guess
         self.switch_player
     end
 
+    def reset_game
+        @losses[@previous_player] += 1
+        puts @previous_player.name + " loses the round!"
+        puts "Fragment: " + @fragment
+        @fragment = ""
+    end
+    
     def run
-        self.play_round until self.lose?
+        until self.game_over?
+            self.print_scoreboard
+            self.play_round until self.lose?
+            self.reset_game
+        end
+
+        puts @current_player.name + " wins the game!"
     end
 
 end
