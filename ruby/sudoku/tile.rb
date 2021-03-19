7c922a8edadd9783da5d9d539028a49d29035800
Jon Dominguez <jon.dominguez94@yahoo.com>
2021-03-18 17:14:03 -0700

    Update tile class with new accessor and render methods

--- ruby/sudoku/tile.rb
+++ ruby/sudoku/tile.rb
@@ -1,8 +1,22 @@
+require 'colorize'
+
 class Tile
+    attr_reader :given, :value
 
     def initialize(value)
         @value = value
         @given = value != 0
+    end
+
+    def value=(num)
+        raise 'Cannot change given values' if @given
+        @value = num
+    end
+
+    def render
+        color = (@value == 0 ? :red : :blue)
+        color = :green if @given
+        puts @value.to_s.colorize(color)
     end
     
 end
