d1a27080b737d10d9ac91edabf1d82c859a4e182
jon-dominguez94 <jon.dominguez94@yahoo.com>
2021-03-26 20:53:06 -0700

    Finish get value logic

--- ruby/sudoku/game.rb
+++ ruby/sudoku/game.rb
@@ -8,11 +8,42 @@ class Game
     
     def play
         until @board.won?
-            pos = self.get_position
+            @board.render
+            pos = self.get_open_position
             val = self.get_value
             @board[pos] = val
         end
         puts "Board Solved!"
+    end
+
+    protected
+
+    def get_value
+        val = nil
+        until self.valid_value?(val) 
+            puts "Enter a value to place in that position (1-9): "
+            val = gets.chomp
+        end
+        val.to_i
+    end
+
+    def valid_value?(val)
+        return false if val.nil?
+        ('1'..'9').to_a.include?(val)
+    end
+    
+    def valid_input?(inp)
+        return false if inp.nil? || inp.split.length != 2
+        inp.split.all? {|parts| ('0'..'8').to_a.include?(parts)}
+    end
+
+    def open_position?(pos)
+        return false if pos.nil?
+        if @board[pos].given
+            puts 'Given positions cannot be changed'
+            return false
+        end
+        true 
     end
 
     def get_position
@@ -24,26 +55,12 @@ class Game
         inp.split.map(&:to_i)
     end
 
-    def get_value
-    end
-
-    def open_position?(pos)
-        if @board[pos].given
-            puts 'Given positions cannot be changed'
-            return false
+    def get_open_position
+        pos = nil
+        until self.open_position?(pos)
+            pos = self.get_position
         end
-        true 
-    end
-
-    def render
-        @board.render
-    end
-
-    protected
-
-    def valid_input?(inp)
-        return false if inp.nil? || inp.split.length != 2
-        inp.split.all? {|parts| ('0'..'8').to_a.include?(parts)}
+        pos
     end
 
 end
